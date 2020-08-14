4de655379783189f8b83dd720069f88818d719a1
parthfloyd <parth6606@gmail.com>
2020-08-14 19:30:34 +0530

    nodeJS Practice

---
+++ nodeJS/task-manager/mongodb.js
@@ -0,0 +1,20 @@
+//CRUD
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL,{useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
+    if(error) {
+        return console.log('Unable to connect to the database!');
+    }
+
+    const db = client.db(databaseName);
+    
+    db.collection('users').insertOne({
+        name: "Parth",
+        age: 24
+    });
+});
